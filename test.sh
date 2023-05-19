144636d4a342e5566d7771285dbe63a063a78465
hossein arabnia <hossein.arabnia@gmail.com>
2023-05-19 22:16:11 +0000

    new test.sh file that contain file and 2 empty file

---
+++ test.sh
@@ -0,0 +1,7 @@
+# this file is only for testing git Behavior!
+# even number is related to local 192.168.100.42
+# and after edit i push to GitHub
+number1: my name is hosein
+number3: I am preparing for a new job
+number5: I am 25 years old!
+number7: I am young and smart!
